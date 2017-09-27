998d108e4416253650a7e16a2af1e557e37fd659
Ayvytr <ayvytr@outlook.com>
2017-09-27 09:34:38 +0800

    1.修改Toast.kt 2.修改gradle属性 3.更新EasyKotlin版本为0.2.0

--- app/src/main/java/com/ayvytr/easykotlinproject/MainActivity.kt
+++ app/src/main/java/com/ayvytr/easykotlinproject/MainActivity.kt
@@ -2,6 +2,8 @@ package com.ayvytr.easykotlinproject
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.ayvytr.easykotlin.context.toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity()
 {
@@ -10,5 +12,21 @@ class MainActivity : AppCompatActivity()
     {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        toast("这是我的Toast，哈哈！")
+//        toast("这是我的Toast，哈哈！1")
+//        toast("这是我的Toast，哈哈！2")
+//        Toast.makeText(this, "Toast1", Toast.LENGTH_SHORT).show()
+//        Toast.makeText(this, "Toast2", Toast.LENGTH_SHORT).show()
+//        Toast.makeText(this, "Toast3", Toast.LENGTH_SHORT).show()
+
+        btn1.setOnClickListener {
+            //            Toast.makeText(this, "Toast1", Toast.LENGTH_SHORT).show()
+            toast("这是我的Toast，哈哈！1")
+        }
+        btn2.setOnClickListener {
+            //            Toast.makeText(this, "Toast2", Toast.LENGTH_SHORT).show()
+            toast("这是我的Toast，哈哈！2")
+        }
     }
 }
