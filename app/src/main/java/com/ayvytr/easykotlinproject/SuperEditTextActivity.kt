ad06a85ad45648ae763ab3ea0521db82abb34712
Ayvytr <ayvytr@outlook.com>
2018-08-25 18:56:44 +0800

    添加SuperEditText

---
+++ app/src/main/java/com/ayvytr/easykotlinproject/SuperEditTextActivity.kt
@@ -0,0 +1,13 @@
+package com.ayvytr.easykotlinproject
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class SuperEditTextActivity : AppCompatActivity()
+{
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_super_edit_text)
+    }
+}
