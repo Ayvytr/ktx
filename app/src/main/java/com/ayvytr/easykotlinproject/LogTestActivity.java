30293d78d335326aed41fa4d0a1236a6234cf385
Ayvytr <ayvytr@outlook.com>
2018-10-06 14:17:40 +0800

    修改代码错误

--- app/src/main/java/com/ayvytr/easykotlinproject/LogTestActivity.java
+++ app/src/main/java/com/ayvytr/easykotlinproject/LogTestActivity.java
@@ -3,7 +3,7 @@ package com.ayvytr.easykotlinproject;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-import com.ayvytr.easykotlin.log.L;
+import com.ayvytr.logger.L;
 
 import java.util.ArrayList;
 
