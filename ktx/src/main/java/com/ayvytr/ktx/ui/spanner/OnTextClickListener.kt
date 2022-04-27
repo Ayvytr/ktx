b251d60163280769e23dd07886e33ad427100913
wangdunwei <ayvytr@163.com>
2022-04-27 17:30:27 +0800

    删除ktx模块，只支持androidx（v4包，com.arch包无法继续使用）

--- ktx/src/main/java/com/ayvytr/ktx/ui/spanner/OnTextClickListener.kt
+++
@@ -1,5 +0,0 @@
-package com.ayvytr.ktx.ui.spanner
-
-interface OnTextClickListener {
-    fun onClicked(text: CharSequence, range: Range, tag: Any?)
-}
--- ktx/src/main/java/com/ayvytr/ktx/ui/spanner/OnTextLongClickListener.kt
+++
@@ -1,5 +0,0 @@
-package com.ayvytr.ktx.ui.spanner
-
-interface OnTextLongClickListener {
-    fun onLongClicked(text: CharSequence, range: Range, tag: Any?)
-}
