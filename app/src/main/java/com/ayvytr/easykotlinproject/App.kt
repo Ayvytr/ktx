415fa6d99d76e2eb2d3984e7f1bd06c268ad9c71
wangdunwei <ayvytr@163.com>
2022-08-03 10:05:14 +0800

    更新依赖

--- app/src/main/java/com/ayvytr/easykotlinproject/App.kt
+++ app/src/main/java/com/ayvytr/easykotlinproject/App.kt
@@ -1,12 +1,8 @@
 package com.ayvytr.easykotlinproject
 
-import android.app.Activity
 import android.app.Application
 import android.content.res.Configuration
-import android.os.Bundle
 import com.ayvytr.ktx.app.ActivityStack
-import com.ayvytr.ktx.context.isNightMode
-import com.ayvytr.logger.L
 
 /**
  * @author EDZ
